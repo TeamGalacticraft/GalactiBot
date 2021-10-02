6b85e197dc5a109d06b33bd4530f9c9642e01707
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2021-10-01 19:41:12 -0700

    chore: initial bot structure

---
+++ build.gradle.kts
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2021-2021 Team Galacticraft
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import java.time.Year
+import java.time.format.DateTimeFormatter
+
+plugins {
+    java
+    id("io.quarkus") version("2.2.3.Final")
+    id("org.cadixdev.licenser") version("0.6.1")
+}
+
+val quarkusVersion = project.property("quarkus.version").toString()
+val quarkusGithubVersion = project.property("quarkus_github.version").toString()
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+base.archivesName.set(project.property("name").toString())
+version = project.property("version").toString()
+group = project.property("group").toString()
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:${quarkusVersion}"))
+    implementation("io.quarkus:quarkus-arc")
+    implementation("io.quarkiverse.githubapp:quarkus-github-app:${quarkusGithubVersion}")
+
+    testImplementation("io.quarkus:quarkus-junit5")
+}
+
+tasks.jar {
+    from("LICENSE")
+    manifest {
+        attributes(
+            "Implementation-Title"     to project.name,
+            "Implementation-Version"   to project.version,
+            "Implementation-Vendor"    to "Team Galacticraft",
+            "Implementation-Timestamp" to DateTimeFormatter.ISO_DATE_TIME,
+            "Maven-Artifact"           to "${project.group}:${project.name}:${project.version}"
+        )
+    }
+}
+
+license {
+    setHeader(project.file("LICENSE_HEADER.txt"))
+    include("**/dev/galacticraft/**/*.java")
+    include("build.gradle.kts")
+    ext {
+        set("year", Year.now().value)
+        set("company", "Team Galacticraft")
+    }
+}
+
+tasks.withType(JavaCompile::class) {
+    dependsOn(tasks.checkLicenses)
+    options.encoding = "UTF-8"
+}
